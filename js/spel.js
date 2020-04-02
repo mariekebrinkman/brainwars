084dced6aead5d428fdc660206195f22772bc442
marieke Brinkman <marieke.brinkman@ziggo.nl>
2020-04-02 08:43:52 +0200

    initial version

---
+++ js/spel.js
@@ -0,0 +1,21 @@
+//globale variabelen
+
+function start() {
+    
+}
+
+function vulBord() {
+
+}
+function toonTijd() {
+
+}
+function interval() {
+
+}
+function eindeSpel() {
+
+}
+
+// en de rest van de functies
+
